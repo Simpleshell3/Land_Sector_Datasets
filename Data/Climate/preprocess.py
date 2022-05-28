27e505c8087df03baafbda0a7163c65bd5e7a8c2
Simpleshell3 <92247771+Simpleshell3@users.noreply.github.com>
2022-05-28 04:04:58 -0500

    Update preprocess.py

--- Data/Climate/preprocess.py
+++ Data/Climate/preprocess.py
@@ -1,4 +1,17 @@
-pip install gdal-utils
-wget https://esdac.jrc.ec.europa.eu/projects/RenewableEnergy/Data/Climate_Zone.zip
-unzip Climate_Zone.zip
+import sys
+import subprocess
+import wget
+import zipfile
+
+# implement pip as a subprocess:
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'<gdal-utils>'])
+
+wget.download(https://esdac.jrc.ec.europa.eu/projects/RenewableEnergy/Data/Climate_Zone.zip)
+
+
+zipfile.Zipfile(Climate_Zone.zip).extractall()
+
+zipfile.Zipfile(Climate_Zone.zip).close()
+
 gdalwarp -s_srs EPSG:4326 -t_srs EPSG:4326 -dstnodata 255.0 -tr 0.05 0.05 -r near -te -180.0 -90.0 180.0 90.0 -te_srs EPSG:4326 -of GTiff "CLIMATE_ZONE.rst" "IPCC_ClimateZoneMap.tif"
