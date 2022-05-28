792fd8462527d664a88a4dc06164d7914efbe190
Simpleshell3 <92247771+Simpleshell3@users.noreply.github.com>
2022-05-28 06:39:13 -0500

    Update preprocess.py

--- Data/Climate/preprocess.py
+++ Data/Climate/preprocess.py
@@ -10,8 +10,8 @@ import zipfile
 wget.download('https://esdac.jrc.ec.europa.eu/projects/RenewableEnergy/Data/Climate_Zone.zip')
 
 
-zipfile.Zipfile('Climate_Zone.zip').extractall()
+zipfile.ZipFile('Climate_Zone.zip').extractall()
 
-zipfile.Zipfile('Climate_Zone.zip').close()
+zipfile.ZipFile('Climate_Zone.zip').close()
 
 #gdalwarp -s_srs EPSG:4326 -t_srs EPSG:4326 -dstnodata 255.0 -tr 0.05 0.05 -r near -te -180.0 -90.0 180.0 90.0 -te_srs EPSG:4326 -of GTiff "CLIMATE_ZONE.rst" "IPCC_ClimateZoneMap.tif"
